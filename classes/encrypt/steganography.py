b19d9930cb08fb9aedda79da6a5ebd162905b2a3
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-04-23 22:53:27 -0700

    decode done

--- classes/encrypt/steganography.py
+++ classes/encrypt/steganography.py
@@ -1,5 +1,4 @@
 from PIL import Image
-import base64
 
     # Loop over the range of x_size (`x`)
         # Loop over the range of y_size (`y`)
@@ -15,31 +14,28 @@ import base64
 def decode_image(file_location):
     encoded_image = Image.open(file_location)
     red_channel = encoded_image.split()[0]
-​
+
     x_size = encoded_image.size[0]
     y_size = encoded_image.size[1]
 ​
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
-​
     #TODO: Fill in decoding functionality
     # Loop over the range of x_size (`x`)
     for x in range(x_size):
         # Loop over the range of y_size (`y`)
         for y in range(y_size):
             # Get RGB values from red_channel via `getpixel(x, y)`
-            r,g,b = red_channel.getpixel((x,y))
+            # Convert red value in returned tuple to a binary string via `bin()`
+            if bin(red_channel.getpixel((x, y)))[-1] == '0':
+                pixels[x, y] = (255, 255, 255)
+            else:
+                pixels[x, y] = (0, 0, 0)
+    decoded_image.save("images/decoded_image.png")
 
-​
 
+decode_image("images/encoded_sample.png")
 
-
-
-    decoded_image.save("images/encoded_sample.png")
-
-#
-#
-#
 # def write_text():
 #     pass
 #
